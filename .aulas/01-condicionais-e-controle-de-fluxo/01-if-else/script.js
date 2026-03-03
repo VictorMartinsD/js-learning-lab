e8e18456a4cf7bd8fb82859ca369c906a412bad8
Victor Martins Dias <victormartinsjob@gmail.com>
2026-03-03 14:32:00 -0300

    refactor: implement login validation using try-catch blocks

--- .aulas/01-condicionais-e-controle-de-fluxo/01-if-else/script.js
+++ .aulas/01-condicionais-e-controle-de-fluxo/01-if-else/script.js
@@ -1,16 +1,21 @@
-function ativarDispositivo(dispositivo, callback) {
-  console.log(`🔄 Acionando: ${dispositivo}...`);
-  callback();
+// Fiz usando o try catch sem que o exercício pedisse
+
+let email = "victor@gmail.com";
+let password = "123";
+let isAdmin = false;
+const validLogin = email === "victor@gmail.com" && password === "123";
+const haveAcess = validLogin || isAdmin;
+
+try {
+  if (typeof email !== "string" || typeof password !== "string") {
+    throw new Error("Tipo de dado incorreto. Digite apenas letras, números e caracteres especiais.");
+  }
+
+  if (!haveAcess) {
+    throw new Error("O e-mail ou a senha está incorreto.");
+  }
+
+  console.log("Login realizado com sucesso!");
+} catch (error) {
+  console.log("Alerta: " + error.message);
 }
-
-function confirmacaoSimples() {
-  console.log("✅ Dispositivo ativado com sucesso!");
-}
-
-ativarDispositivo("Lâmpada", confirmacaoSimples);
-
-ativarDispositivo("Alarme", function () {
-  console.log("🚨 Alarme tocando! Atenção!");
-});
-
-ativarDispositivo("Câmera", () => console.log("📸 Foto capturada!"));
