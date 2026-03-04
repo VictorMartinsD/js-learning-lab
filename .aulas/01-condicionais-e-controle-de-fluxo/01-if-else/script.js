04f05ba8095e7be7453a78a692674372fae99841
Victor Martins Dias <victormartinsjob@gmail.com>
2026-03-04 20:04:02 -0300

    refactor: optimize boolean logic and implement type checking

--- .aulas/01-condicionais-e-controle-de-fluxo/01-if-else/script.js
+++ .aulas/01-condicionais-e-controle-de-fluxo/01-if-else/script.js
@@ -1,21 +1,27 @@
-// Fiz usando o try catch sem que o exercício pedisse
-
 let email = "victor@gmail.com";
 let password = "123";
-let isAdmin = false;
-const validLogin = email === "victor@gmail.com" && password === "123";
-const haveAcess = validLogin || isAdmin;
+let validEmail = "victor@gmail.com";
+let validPassword = "123";
+let isAdmin = true;
+const validation = email === validEmail && password === validPassword;
+const haveAcess = validation || isAdmin;
 
 try {
   if (typeof email !== "string" || typeof password !== "string") {
-    throw new Error("Tipo de dado incorreto. Digite apenas letras, números e caracteres especiais.");
+    throw new Error("Tipo de dado inválido, digite letras, números e caracteres especiais.");
   }
 
   if (!haveAcess) {
-    throw new Error("O e-mail ou a senha está incorreto.");
+    throw new Error("O e-mail ou a senha estão incorretos.");
   }
 
-  console.log("Login realizado com sucesso!");
+  console.log("Acesso liberado!");
 } catch (error) {
   console.log("Alerta: " + error.message);
 }
+
+/*
+Usando try catch para diferenciar
+
+Aprendi que posso usar somente "validation" no lugar de colocar "validation === true"
+*/
