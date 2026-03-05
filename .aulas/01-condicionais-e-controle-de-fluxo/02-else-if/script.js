e6628ac2baec48876a48b49bde31d1fca72188ed
Victor Martins Dias <victormartinsjob@gmail.com>
2026-03-04 21:06:07 -0300

    refactor: simplify conditional logic and standardize code blocks

--- .aulas/01-condicionais-e-controle-de-fluxo/02-else-if/script.js
+++ .aulas/01-condicionais-e-controle-de-fluxo/02-else-if/script.js
@@ -3,8 +3,9 @@ let input = Number("abc");
 if (typeof input !== "number" || Number.isNaN(input) || input < 0 || input > 23) {
   console.log("❌ ERRO: O valor digitado não é um horário válido!");
 } else {
-  if (input <= 12) console.log("Bom dia!");
-  else if (input > 12 && input <= 18) {
+  if (input <= 12) {
+    console.log("Bom dia!");
+  } else if (input <= 18) {
     console.log("Boa tarde!");
   } else {
     console.log("Boa noite!");
