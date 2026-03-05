be877c5206760fc4b21a342a2a9c397a72265f4d
Victor Martins Dias <victormartinsjob@gmail.com>
2026-03-05 11:04:20 -0300

    refactor: rename variables for clarity and update status messages

--- .aulas/01-condicionais-e-controle-de-fluxo/04-try-catch-finally/script.js
+++ .aulas/01-condicionais-e-controle-de-fluxo/04-try-catch-finally/script.js
@@ -1,17 +1,17 @@
-let pesoCarga = 500;
+let input = 500;
 
 try {
-  if (typeof pesoCarga !== "number" || Number.isNaN(pesoCarga)) {
+  if (typeof input !== "number" || Number.isNaN(input)) {
     throw new Error("O valor do peso é inválido (não é um número).");
   }
 
-  if (pesoCarga > 450) {
+  if (input > 450) {
     throw new Error("Peso máximo excedido! O elevador não pode subir.");
   }
 
   console.log("✅ Elevador liberado. Subindo...");
 } catch (error) {
-  console.log("⚠️ ALERTA DO SISTEMA: " + error.message);
+  console.log("Alerta: ", error.message);
 } finally {
   console.log("🚪 Portas fechadas.");
 }
